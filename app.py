b5246a6d4fa78755a0b8d9d95500f3dbb9ee66f0
michaeltrinh <asdf.michael@hotmail.com>
2023-05-09 21:45:09 +1000

    created templates with jinja

--- app.py
+++ app.py
@@ -12,11 +12,6 @@ def index():
     return render_template('base.html')
 
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
