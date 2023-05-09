5e437132c4fbe446d313dc6018702b0259515064
michaeltrinh <asdf.michael@hotmail.com>
2023-05-09 13:56:47 +1000

    testing env variable

---
+++ vases_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS vases
+
+CREATE TABLE vases(id SERIAL PRIMARY KEY, name TEXT NOT NULL, price INT NOT NULL, image TEXT, qty INT NOT NULL) ;
+
+
+INSERT INTO vases(name, price, image, qty) VALUES ();
+INSERT INTO vases(name, price, image, qty) VALUES ();
+INSERT INTO vases(name, price, image, qty) VALUES ();
+INSERT INTO vases(name, price, image, qty) VALUES ();
